e66b79bcea8db23201db588adc9099d0af7059d6
mcasan <31909324+mcasan@users.noreply.github.com>
2017-10-30 21:17:59 -0400

    Add it

---
+++ Project2(Part2)/Assets/Scripts/Cube_Behavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_Behavior : MonoBehaviour {
+	
+public GameObject cubePrep;
+	 
+public	int x;
+
+public	int y;
+
+/// When the mouse is cliked and "it is not" a red cube then it can turn to a white cube. 
+
+	void OnMouseDown () {
+
+
+		if (GameController.redcube != null) {
+
+			GameController.redcube.GetComponent<Renderer> ().material.color = Color.white;
+		
+		}
+
+		//// When a cube is cliked, then, it becomes a red cube. 
+ 
+		gameObject.GetComponent<Renderer> ().material.color = Color.red;
+
+		GameController.redcube = gameObject;
+
+////  When the red cube of the top left of the game it is not a red cube anymore, then, it becomes a blue cube. 
+
+		if (gameObject == GameController.redcube && GameController.cliked == false) {	
+
+			gameObject.GetComponent<Renderer> ().material.color = Color.blue;
+
+			GameController.cliked = true;
+	
+
+
+		}
+
+
+	}
+}
+
+
---
+++ Project2(Part2)/Assets/Scripts/GameController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public static int Xairplane;
+
+	public static int Yairplane;
+
+	public GameObject currentCube;
+
+	public GameObject cubePre;
+
+	public static GameObject redcube;
+
+	public static bool cliked;
+
+	GameObject[,]cubeRow;
+
+	Vector3 cubePosition;
+
+
+
+
+	// Use this for initialization
+
+
+	/// This is to position the cubes, declare that "cliked is false", create a position to put a red cube at the top left of the game and, to make cubes no more than 16 x 9.
+	void Start () {
+
+		cliked = false;
+
+		cubeRow = new GameObject[16,9];
+
+		Xairplane = 0;
+
+		Yairplane = 8;
+
+		for (int y = 0; y < 9; y++){
+			
+
+			for (int x = 0; x < 16; x++) {
+
+				////This is goint to make that the cubes stop after going 16 times in one x position and, it is going to stop after 9 times in one y position. 
+		 		cubePosition = new Vector3 (x * 2, y * 2 - 8, 0);
+
+				cubeRow [x, y] = (GameObject)Instantiate (cubePre, cubePosition, Quaternion.identity);
+
+				cubeRow [x, y].GetComponent<Renderer> ().material.color = Color.white;
+
+				cubeRow [x, y].GetComponent<Cube_Behavior> ().x = x;
+
+				cubeRow [x, y].GetComponent<Cube_Behavior> ().y = y;
+
+				//// The position of the red cube is alredy set, this is to create the red cube.
+				if (Xairplane == x && Yairplane == y) {
+
+					cubeRow [x, y].GetComponent<Renderer> ().material.color = Color.red;
+
+					/// I assingned redcube as cubeRow.
+
+					redcube = cubeRow [x, y];
+
+
+
+				}				
+		
+													  }
+
+
+
+
+			
+
+										}
+
+		
+												}
+				
+
+					}
+
+
+	
+	
+	
+
